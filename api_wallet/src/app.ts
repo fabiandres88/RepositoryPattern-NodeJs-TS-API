2c130f9a5153dbfeb8c7fab2aa40615d72d96f34
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-01-22 08:38:26 -0500

    Implementing dotenv module and configuring it

--- api_wallet/src/app.ts
+++ api_wallet/src/app.ts
@@ -1,6 +1,22 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.APP_ENV = process.env.APP_ENV || 'development';
+
+// Env files
+import dotenv = require('dotenv');
+dotenv.config({
+    path: `${__dirname}/../config/${process.env.APP_ENV}.env`
+})
+
+console.log(process.env.APP_FOO);
+
+
 import express = require('express');
 
 const app: express.Application = express();
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Running with ts-node-dev...')
+});
 
 export { app };
--- api_wallet/src/server.ts
+++ api_wallet/src/server.ts
@@ -1,9 +1,5 @@
 import { app } from './app';
 
-app.get('/', (req, res) => {
-    res.send('Running with ts-node-dev...')
-})
-
 app.listen(3000, () => {
     console.log("Server started at port 3000 ...");
     
