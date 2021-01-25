a008dba6868967a66ac33bbda788c3a23a1f04c3
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-01-25 14:20:06 -0500

    Using environment variables

--- api_wallet/src/server.ts
+++ api_wallet/src/server.ts
@@ -1,6 +1,12 @@
 import { app } from './app';
+import "reflect-metadata";
+import { createConnection } from 'typeorm';
 
-app.listen(3000, () => {
-    console.log("Server started at port 3000 ...");
-    
+//database connection
+createConnection().then(async connection => { 
+    console.log(">>> Mysql connection with TypeORM <<<"); 
+  }).catch(error => console.log(error));
+// server connection
+app.listen(process.env.PORT, () => {
+    console.log(`>>> Server running at port: ${process.env.PORT} <<<`);    
 });
